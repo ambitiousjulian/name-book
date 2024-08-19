fd4814a4940c475877003317f0bdb71142464581
ambitiousjulian <juliancajuste@gmail.com>
2024-08-19 08:55:05 -0500

    Initial Commit

---
+++ HeadmanNation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeadmanNation
+//
+//  Created by Julian Cajuste on 8/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeadmanNation/HeadmanNationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadmanNationApp.swift
+//  HeadmanNation
+//
+//  Created by Julian Cajuste on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct HeadmanNationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
